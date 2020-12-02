e949e28053214ea21a4ec4585ed85cf412b8949b
grudus <jakgruda@gmail.com>
2020-12-02 10:52:15 +0100

    Solve day1 in Python

---
+++ src/main/java/com/grudus/adventofcode/day01/day01.py
@@ -0,0 +1,25 @@
+import itertools
+from operator import mul
+from functools import reduce
+
+
+def first_star(expense_report):
+    combined = list(itertools.combinations(expense_report, 2))
+    matching_pairs = [pair for pair in combined if sum(list(pair)) == 2020]
+    return reduce(mul, matching_pairs[0])
+
+
+def second_star(expense_report):
+    combined = list(itertools.combinations(expense_report, 3))
+    matching_pairs = [pair for pair in combined if sum(list(pair)) == 2020]
+    return reduce(mul, matching_pairs[0])
+
+
+
+
+
+report = open('input.txt', 'r').read()
+numbers = list(map(int, report.split("\n")))
+
+print(first_star(numbers))
+print(second_star(numbers))
