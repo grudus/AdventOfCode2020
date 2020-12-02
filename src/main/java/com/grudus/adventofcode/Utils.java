e2ebf4eef3834e5b061cdcea250e9662b29e7f2f
grudus <jakgruda@gmail.com>
2020-12-02 09:04:58 +0100

    Solve day 2

--- src/main/java/com/grudus/adventofcode/Utils.java
+++ src/main/java/com/grudus/adventofcode/Utils.java
@@ -4,7 +4,10 @@ import lombok.SneakyThrows;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Utils {
 
@@ -17,4 +20,17 @@ public class Utils {
         var day = classDay.getSimpleName().substring(3);
         return readFileInput(day);
     }
+
+    public static List<String> findRegexGroups(Pattern pattern, String text) {
+        Matcher matcher = pattern.matcher(text);
+        List<String> groups = new ArrayList<>();
+
+        while (matcher.find()) {
+            for (int i = 1; i <= matcher.groupCount(); i++) {
+                groups.add(matcher.group(i));
+            }
+        }
+        return groups;
+    }
+
 }
---
+++ src/main/java/com/grudus/adventofcode/day02/Day02.java
@@ -0,0 +1,55 @@
+package com.grudus.adventofcode.day02;
+
+import com.grudus.adventofcode.Utils;
+import io.vavr.collection.List;
+
+import java.util.regex.Pattern;
+
+import static io.vavr.collection.List.ofAll;
+import static java.lang.Integer.parseInt;
+
+// --- Day 2: Password Philosophy ---
+public class Day02 {
+    private final static Pattern PASSWORD_POLICY_PATTERN = Pattern.compile("(\\d+)-(\\d+) (\\w): (\\w+)");
+
+    record PolicyAndPassword(int rangeStart, int rangeEnd, char letter, String password) {
+        static PolicyAndPassword fromGroups(String[] groups) {
+            return new PolicyAndPassword(parseInt(groups[0]), parseInt(groups[1]), groups[2].charAt(0), groups[3]);
+        }
+
+        boolean isFirstJobValid() {
+            long count = password.chars()
+                    .filter(character -> character == letter)
+                    .count();
+            return count >= rangeStart && count <= rangeEnd;
+        }
+
+        boolean isOfficialTobogganCorporateAuthenticationSystemValid() {
+            return ((password.charAt(rangeStart - 1) == letter ? 1 : 0)
+                    + (password.charAt(rangeEnd - 1) == letter ? 1 : 0)) == 1;
+        }
+    }
+
+
+    public static int firstStar(List<String> input) {
+        return input
+                .map(line -> Utils.findRegexGroups(PASSWORD_POLICY_PATTERN, line))
+                .map(groups -> PolicyAndPassword.fromGroups(groups.toArray(String[]::new)))
+                .count(PolicyAndPassword::isFirstJobValid);
+    }
+
+    public static int secondStar(List<String> input) {
+        return input
+                .map(line -> Utils.findRegexGroups(PASSWORD_POLICY_PATTERN, line))
+                .map(groups -> PolicyAndPassword.fromGroups(groups.toArray(String[]::new)))
+                .count(PolicyAndPassword::isOfficialTobogganCorporateAuthenticationSystemValid);
+    }
+
+
+    public static void main(String[] args) {
+        var input = Utils.readFileInput(Day02.class);
+
+        System.out.println(firstStar(ofAll(input)));
+        System.out.println(secondStar(ofAll(input)));
+    }
+}
