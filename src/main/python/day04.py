2dc48f1aed739813017e9ae0a2615e171d4334cd
grudus <jakgruda@gmail.com>
2020-12-04 13:03:47 +0100

    Fix Python's solution to the 4th day

--- src/main/python/day04.py
+++ src/main/python/day04.py
@@ -3,6 +3,11 @@ import re
 def first_star(passports):
     return len([passport for passport in passports if is_valid_passport(passport) ])
 
+def second_star(passports):
+    valid_passports = list(filter(is_valid_passport, passports))
+    passport_maps = [create_passport_map(passport.split()) for passport in valid_passports]
+    return len(list(filter(has_valid_values, passport_maps)))
+
 
 def is_valid_passport(passport):
     return has_required_keys(extract_keys(passport.split()))
@@ -13,12 +18,6 @@ def extract_keys(passport):
 def has_required_keys(password_key):
     return len([key for key in password_key if key != 'cid']) == 7
 
-
-def second_star(passports):
-    valid_passports = list(filter(is_valid_passport, passports))
-    passport_maps = [create_passport_map(passport.split()) for passport in valid_passports]
-    return len(list(filter(has_valid_values, passport_maps)))
-
 def create_passport_map(passport):
     return {x.split(":")[0]: x.split(":")[1] for x in passport}
 
@@ -28,25 +27,21 @@ def has_valid_values(passport_map):
     and (2010 <= int(passport_map['iyr']) <= 2020)
     and (2020 <= int(passport_map['eyr']) <= 2030)
     and (has_valid_height(passport_map['hgt']))
-    and (bool(re.match(r'^#(?:[0-9a-f]{6})$', passport_map['hcl'])))
-    and (bool(re.match(r"amb|blu|brn|gry|grn|hzl|oth", passport_map['ecl'])))
-    and (len(passport_map['pid']) == 9)
+    and (bool(re.match(r'^#([0-9a-f]{6})$', passport_map['hcl'])))
+    and (passport_map['ecl'] in "amb blu brn gry grn hzl oth".split())
+    and (bool(re.match(r'^[0-9]{9}$', passport_map['pid'])))
     )
 
-
 def has_valid_height(height):
-    parsed_data = re.findall(r'(\d+)(cm|in)', height)
-
-    if len(parsed_data) == 0 or len(parsed_data[0]) < 2:
+    if height[-2:] == 'cm':
+        return 150 <= int(height[:-2]) <= 193
+    elif height[-2:] == 'in':
+        return 59 <= int(height[:-2]) <= 76
+    else:
         return False
 
-    (height, unit) = parsed_data[0]
-    return unit == 'cm' if (150 <= int(height) <= 193) else (59 <= int(height) <= 76)
 
-
-passports = open('src/main/resources/day04/input.txt', 'r').read().replace(",", "").split("\n\n")
-
-# print(passports[0])
-
-print(first_star(passports))
-print(second_star(passports))
+if __name__ == "__main__":
+    passports = open('src/main/resources/day04/input.txt', 'r').read().split("\n\n")
+    print(first_star(passports))
+    print(second_star(passports))
