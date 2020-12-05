6a2345d2d8d34ed35a6051220288c00ad3a99a7c
grudus <jakgruda@gmail.com>
2020-12-05 12:27:12 +0100

    Solve day5 in Python

--- src/main/python/day05.py
+++ src/main/python/day05.py
@@ -1,14 +1,27 @@
 
 def first_star(boarding_passes):
     positions = [calculate_position(boarding_pass) for boarding_pass in boarding_passes]
-    return max([row * 8 + col for (row, col) in positions])
+    return max([calculate_id(position) for position in positions])
 
+
+def second_star(boarding_passes):
+    positions = [calculate_position(boarding_pass) for boarding_pass in boarding_passes]
+    position_ids = sorted(map(calculate_id, positions))
+
+    previous_id = position_ids[0]
+    for position_id in position_ids[1:]:
+
+        if position_id != (previous_id + 1):
+            return position_id - 1
+        
+        previous_id = position_id
+
+
+def calculate_id(tuple):
+    return tuple[0] * 8 + tuple[1]
 
 def calculate_position(boarding_pass):
-    rows = boarding_pass[:7]
-    cols = boarding_pass[7:]
-
-    return (convert_to_binary(rows), convert_to_binary(cols))
+    return (convert_to_binary(boarding_pass[:7]), convert_to_binary(boarding_pass[7:]))
 
 def convert_to_binary(position_stirng):
     binary_list = ["1" if position in ['B', 'R'] else "0" for position in position_stirng]
@@ -19,4 +32,4 @@ def convert_to_binary(position_stirng):
 if __name__ == "__main__":
     boarding_passes = open('src/main/resources/day05/input.txt', 'r').read().split("\n")
     print(first_star(boarding_passes))
-    # print(second_star(boarding_passes))
+    print(second_star(boarding_passes))
