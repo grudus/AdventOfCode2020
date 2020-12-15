e3976ded5fb94a4d65c8a91c55a73ace306d112c
grudus <jakgruda@gmail.com>
2020-12-15 09:05:16 +0100

    Solve day15 in Python

---
+++ src/main/python/day15.py
@@ -0,0 +1,31 @@
+from collections import defaultdict
+
+def first_star(starting_numbers): return find_nth_spoken_number(starting_numbers, 2020)
+def second_star(starting_numbers): return find_nth_spoken_number(starting_numbers, 30000000)
+
+def find_nth_spoken_number(starting_numbers, n):
+    occurences = defaultdict(list)
+    for i, number in enumerate(starting_numbers): occurences[number] = [i + 1]
+    
+    turn = len(starting_numbers) + 1
+    prev_number = starting_numbers[-1]
+
+    while turn <= n:
+        prev_occurences = occurences[prev_number]
+        
+        if len(prev_occurences) < 2:
+            prev_number = 0
+            occurences[prev_number] += [turn]
+        else:
+            diff = prev_occurences[-1] - prev_occurences[-2]
+            prev_number = diff
+            occurences[diff] += [turn]
+        turn += 1
+    
+    return prev_number
+
+
+if __name__ == "__main__":
+    starting_numbers = [int(x) for x in open('src/main/resources/day15/input.txt', 'r').read().split(",")]
+    print(first_star(starting_numbers))
+    print(second_star(starting_numbers))
