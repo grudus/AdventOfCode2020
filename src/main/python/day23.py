6ce5ff6b3d1848016aa8a5e5bc07290ea44e2d6a
grudus <jakgruda@gmail.com>
2020-12-29 15:24:50 +0100

    Solve day23 in Python

--- src/main/python/day23.py
+++ src/main/python/day23.py
@@ -1,41 +1,78 @@
-def first_star(cups):
-    current_cup = cups[0]
-    for _ in range(100): cups, current_cup = make_move(cups[:], current_cup)
-    return calculate_score(cups)
-
-
-def make_move(cups, current_cup):
-    normalize_dest = lambda x: x if x >= min(cups) else max(cups)
-
-    current_index = cups.index(current_cup)
-    picked = []
-    index_to_pick = current_index + 1
-
-    while len(picked) != 3:
-        index_to_pick = index_to_pick % len(cups)
-        picked.append(cups[index_to_pick])
-        del cups[index_to_pick]
-
-    destination = normalize_dest(current_cup - 1)
-    while destination in picked: destination = normalize_dest(destination - 1)
-    destination_index = cups.index(destination)
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
     
-    picked = list(reversed(picked))
-    for index_to_pick in range(destination_index+1, destination_index+4):
-        cups.insert(index_to_pick, picked.pop())
+    def __repr__(self): return str(self.data)
 
-    return cups, cups[(cups.index(current_cup) + 1) % len(cups)]
+class LinkedCircularList:
+    def __init__(self, nodes):
+        self.dataMap = {}
+        self.maxValue = max(nodes)
+        self.minValue = min(nodes)
+        
+        node = Node(data=nodes.pop(0))
+        self.head = node
+        self.dataMap[node.data] = node
+
+        for elem in nodes:
+            node.next = Node(data=elem)
+            node = node.next
+            self.dataMap[elem] = node
+
+        node.next = self.head # circular loop
+
+    def __repr__(self):
+        node = self.head
+        first_value = node.data
+        nodes = [str(first_value)]
+        node = node.next
+        while node.data != first_value: # prevent infinite loop
+            nodes.append(str(node.data))
+            node = node.next
+
+        return "".join(nodes)
+
+    def find_smaller_value(self, node: Node, values_to_ignore) -> Node:
+        decrease_in_range = lambda value: value - 1 if value > self.minValue else self.maxValue
+    
+        searched = decrease_in_range(node.data)
+        while searched in values_to_ignore: searched = decrease_in_range(searched)
+        return self.dataMap[searched]
 
 
-def calculate_score(cups):
-    starting_index = cups.index(1)
-    rearranged_cups = (cups + cups)[starting_index + 1:starting_index + len(cups)]
-    return "".join(map(str, rearranged_cups))
 
+def first_star(cups):
+    linked_cups = play_a_game(cups, 100)
+    linked_cups.head = linked_cups.dataMap[1]
+    return str(linked_cups)[1:]
+
+def second_star(cups):
+    cups = cups + list(range(max(cups) + 1, 1000000 + 1))
+    linked_cups = play_a_game(cups, 10000000)
+    node_1 = linked_cups.dataMap[1]
+    return node_1.next.data * node_1.next.next.data
+
+def play_a_game(cups, number_of_plays) -> LinkedCircularList:
+    linked_cups = LinkedCircularList(cups)
+    current_cup = linked_cups.head
+    for _ in range(number_of_plays): current_cup = move_cups(linked_cups, current_cup)
+    return linked_cups
+
+
+def move_cups(cups: LinkedCircularList, current_cup: Node):
+    picked = [current_cup.next, current_cup.next.next, current_cup.next.next.next]
+    current_cup.next = picked[-1].next
+
+    destination = cups.find_smaller_value(current_cup, values_to_ignore=[cup.data for cup in picked])
+    picked[-1].next = destination.next
+    destination.next = picked[0]
+    
+    return current_cup.next
 
 
 if __name__ == "__main__":
     cups = list(open('src/main/resources/day23/input.txt', 'r').read())
     cups = [int(cup) for cup in cups]
     print(first_star(cups[:]))
-    # print(second_star(player1_deck, player2_deck))
+    print(second_star(cups[:]))
