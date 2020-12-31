4c5104897062c273b7c260e75568553c5c9c31d0
grudus <jakgruda@gmail.com>
2020-12-31 17:32:35 +0100

    Refactor first part of the 20th day; poc of 2nd part

--- src/main/python/day20.py
+++ src/main/python/day20.py
@@ -1,23 +1,78 @@
 import itertools
 from collections import defaultdict
 from functools import reduce
+import typing
+import dataclasses
+
+Id = int
+Area = typing.Text
+
+@dataclasses.dataclass
+class Borders:
+    top: typing.Tuple[int, int]
+    right: typing.Tuple[int, int]
+    bottom: typing.Tuple[int, int]
+    left: typing.Tuple[int, int]
+
+    def to_set(self):
+        return set(self.top) | set(self.right) | set(self.bottom) | set(self.left)
+
 
 def first_star(tiles_input):
-    tiles = [(tile_line.split("\n")[0], find_borders(tile_line.split("\n")[1:])) for tile_line in tiles_input]
-    
-    border_count = defaultdict(int)
-
-    for _, borders in tiles:
-        for border in borders: border_count[border] += 1
-    
-    corner_tiles = [int(tile_id[5:-1]) 
-        for tile_id, borders in tiles 
-        if len([1 for border in borders if border_count[border] == 1]) == 4]
-    
+    tiles = parse_tiles_input(tiles_input)
+    tile_to_neighbours = find_neighbours(tiles)
+    corner_tiles = [tile_id for tile_id, neighbours in tile_to_neighbours.items() if len(neighbours) == 2]
     return reduce(int.__mul__, corner_tiles)
 
 
-def find_borders(tile_lines):
+def second_star(tiles_input):
+    tiles = parse_tiles_input(tiles_input)
+    tile_to_neighbours = find_neighbours(tiles)
+    world_map = {}
+
+    for tile_id, neighbours in tile_to_neighbours.items():
+        if not world_map and len(neighbours) == 2: # first corner 
+            
+            area, borders = tiles[tile_id]
+            world_map[(0, 0)] = area
+            
+            print(borders)
+            
+
+            for neighbour in neighbours:
+                n_area, n_borders = tiles[neighbour]
+                print(n_borders)
+            
+            break
+
+    for coord, area in world_map.items():
+        for row in area:
+            print(row)
+        print()
+
+
+
+def find_neighbours(tiles: typing.Dict[Id, typing.Tuple[Area, Borders]]) -> typing.DefaultDict[Id, typing.List[Id]]:
+    tile_to_neighbours = defaultdict(list)
+
+    for tile_id, (_, borders) in tiles.items():
+
+        for tile2_id, (_, borders2) in tiles.items():
+            common_borders = borders.to_set() & borders2.to_set()
+            if common_borders and tile_id != tile2_id: tile_to_neighbours[tile_id].append(tile2_id)
+
+    return tile_to_neighbours
+
+
+def parse_tiles_input(tiles_input: typing.List[str]) -> typing.Dict[Id, typing.Tuple[Area, Borders]]:
+    return {int(tile_id[5:-1]): (area, find_borders(area)) 
+        for tile_id, *area in 
+            [single_area.split("\n") for single_area in tiles_input]
+        }
+
+
+
+def find_borders(tile_lines) -> Borders:
     to_binary = lambda line: int("".join(["1" if tile == '#' else "0" for tile in line]), 2)
 
     top = tile_lines[0]
@@ -25,13 +80,15 @@ def find_borders(tile_lines):
     left = [line[0] for line in tile_lines]
     right = [line[-1] for line in tile_lines]
 
-    return [to_binary(top), to_binary(top[::-1]),
-        to_binary(right), to_binary(right[::-1]),
-        to_binary(bottom), to_binary(bottom[::-1]), 
-        to_binary(left), to_binary(left[::-1])]
-
+    return Borders(
+        (to_binary(top), to_binary(top[::-1])),
+        (to_binary(right), to_binary(right[::-1])),
+        (to_binary(bottom), to_binary(bottom[::-1])), 
+        (to_binary(left), to_binary(left[::-1]))
+    )
 
 
 if __name__ == "__main__":
     tiles_input = open('src/main/resources/day20/input.txt', 'r').read().split("\n\n")
     print(first_star(tiles_input))
+    print(second_star(tiles_input))
